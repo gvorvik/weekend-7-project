b0914671f60366c1de574b12840a7732f6a6dce4
Greg Orvik <gvorvik@gmail.com>
2018-05-26 17:20:52 -0500

    successfully displays data from get request on admin table

--- src/components/admin/adminTable/adminTable.js
+++ src/components/admin/adminTable/adminTable.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class AdminTable extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            feedback: [],
+        };
+    }
+
+    getFeedbackData = () => {
+        console.log('get feedback data ran');
+        axios({
+            method: 'GET',
+            url: '/feedback'
+        })
+        .then((response) => {
+            this.setState({
+                feedback: response.data
+            });
+            console.log(this.state.feedback);
+        })
+        .catch((error) => {
+            console.log('error with axios get', error);
+        })
+    }
+
+    componentDidMount() {
+        this.getFeedbackData();
+    };
+
     render() {
+
+        let tableRows = this.state.feedback.map((feedback) => {
+            return <tr key={feedback.id}><td>{feedback.id}</td><td>{feedback.feeling}</td><td>{feedback.understanding}</td>
+            <td>{feedback.support}</td><td>{feedback.comments}</td></tr>;
+        })
+
         return (
             <div>
                 <table>
@@ -16,13 +52,7 @@ class AdminTable extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>3</td>
-                            <td>4</td>
-                            <td>5</td>
-                            <td>Everything went really well!</td>
-                        </tr>
+                        {tableRows}
                     </tbody>
                 </table>
             </div>
